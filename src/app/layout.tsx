f3e05e9fd1a2c33d9761cf031e31b823b3f5105a
Francisco <github.backstage538@passmail.net>
2024-11-24 14:25:58 +0100

    Install shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import Link from 'next/link';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { homePath, ticketsPath } from '@/paths';
 
 const geistSans = localFont({
@@ -30,19 +31,25 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav className='supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between'>
+        <nav className='supports-backdrop-blur:bg-background/60 bg-background/95 fixed left-0 right-0 top-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur'>
           <div>
-            <Link href={homePath} className='text-lg font-bold'>
+            <Link
+              href={homePath}
+              className={buttonVariants({ variant: 'outline' })}
+            >
               Home
             </Link>
           </div>
           <div>
-            <Link href={ticketsPath} className='text-sm underline'>
+            <Link
+              href={ticketsPath}
+              className={buttonVariants({ variant: 'outline' })}
+            >
               Tickets
             </Link>
           </div>
         </nav>
-        <main className='min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 bg-secondary/20 flex flex-col'>
+        <main className='bg-secondary/20 flex min-h-screen flex-1 flex-col overflow-y-auto overflow-x-hidden px-8 py-24'>
           {children}
         </main>
       </body>
