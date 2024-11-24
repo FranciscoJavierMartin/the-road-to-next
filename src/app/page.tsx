db18977482363b50551266a5901d20119772eb9c
Francisco <github.backstage538@passmail.net>
2024-11-24 14:07:31 +0100

    Styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,18 @@ import { ticketsPath } from '@/paths';
 
 export default function Home() {
   return (
-    <div>
-      <h2 className='text-lg'>HomePage</h2>
-      <Link href={ticketsPath} className='underline'>
-        Go to tickets
-      </Link>
+    <div className='flex-1 flex flex-col gap-y-8'>
+      <div>
+        <h2 className='text-3xl font-bold tracking-tight'>HomePage</h2>
+        <p className='text-muted-foreground text-sm'>
+          Your home place to start
+        </p>
+      </div>
+      <div className='flex flex-1 flex-col items-center'>
+        <Link href={ticketsPath} className='underline'>
+          Go to tickets
+        </Link>
+      </div>
     </div>
   );
 }
