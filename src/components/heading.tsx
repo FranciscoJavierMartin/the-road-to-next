c8ec996dad991a39ec2359e0fe5c738402a73bfc
Francisco <github.backstage538@passmail.net>
2024-12-03 20:02:46 +0100

    Add tabs

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,13 +1,16 @@
-import { Separator } from './ui/separator';
+import { ReactNode } from 'react';
+import { Separator } from '@/components/ui/separator';
 
 type HeadingProps = {
   title: string;
   description?: string;
+  tabs?: ReactNode;
 };
 
-export default function Heading({ title, description }: HeadingProps) {
+export default function Heading({ title, description, tabs }: HeadingProps) {
   return (
     <>
+      {tabs}
       <div>
         <h2 className='text-3xl font-bold tracking-tight'>{title}</h2>
         {description && (
--- src/components/sidebar/types.ts
+++ src/components/sidebar/types.ts
@@ -1,6 +1,7 @@
 import { ReactElement } from 'react';
 
 export type NavItem = {
+  separator?: boolean;
   title: string;
   icon: ReactElement;
   href: string;
