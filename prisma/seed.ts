977d50718497d7e3d4b67553aa4b67a2357e666f
Francisco <github.backstage538@passmail.net>
2024-12-05 22:29:38 +0100

    Add comment to db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -38,8 +38,18 @@ const tickets = [
   },
 ];
 
+const comments = [
+  { content: 'First comment from DB.' },
+  { content: 'Second comment from DB.' },
+  { content: 'Third comment from DB.' },
+];
+
 async function seed(): Promise<void> {
-  await Promise.all([prisma.ticket.deleteMany(), prisma.user.deleteMany()]);
+  await Promise.all([
+    prisma.comment.deleteMany(),
+    prisma.ticket.deleteMany(),
+    prisma.user.deleteMany(),
+  ]);
   const passwordHash = await hash('adminadmin');
   const dbUsers = await prisma.user.createManyAndReturn({
     data: users.map((user) => ({
@@ -47,8 +57,16 @@ async function seed(): Promise<void> {
       passwordHash,
     })),
   });
-  await prisma.ticket.createMany({
+  const dbTickets = await prisma.ticket.createManyAndReturn({
     data: tickets.map((ticket) => ({ ...ticket, userId: dbUsers[0].id })),
+  });
+
+  await prisma.comment.createMany({
+    data: comments.map((comment) => ({
+      ...comment,
+      ticketId: dbTickets[0].id,
+      userId: dbUsers[1].id,
+    })),
   });
 }
 
